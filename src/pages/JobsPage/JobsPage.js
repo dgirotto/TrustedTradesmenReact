b158ab06b998320f4a86285f5f86b6170cc96aa1
Daniel Girotto <dgirotto@remaxintegra.com>
2020-08-02 04:09:06 -0400

    Update job table styling

--- src/pages/JobsPage/JobsPage.js
+++ src/pages/JobsPage/JobsPage.js
@@ -19,46 +19,22 @@ import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import Collapse from "@material-ui/core/Collapse";
 import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
-
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-//   createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-//   createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5)
-// ];
-
-// function createData(name, calories, fat, carbs, protein, price) {
-//   return {
-//     name,
-//     calories,
-//     fat,
-//     carbs,
-//     protein,
-//     price,
-//     history: [
-//       { date: "2020-01-05", customerId: "11091700", amount: 3 },
-//       { date: "2020-01-02", customerId: "Anonymous", amount: 1 }
-//     ]
-//   };
-// }
+import Button from "@material-ui/core/Button";
 
 function Row(props) {
   const { row } = props;
   const [open, setOpen] = React.useState(false);
-  // const classes = useRowStyles();
+
   return (
     <React.Fragment>
-      {/* <TableRow className={classes.root}> */}
       <TableRow onClick={() => setOpen(!open)} style={{ cursor: "pointer" }}>
         <TableCell>
           <IconButton aria-label="expand row" size="small">
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell>{row.serviceId}</TableCell>
+        <TableCell>{row.serviceName}</TableCell>
         <TableCell>{row.address}</TableCell>
         <TableCell>{row.city}</TableCell>
         <TableCell>{row.creationDate.split(" ")[0]}</TableCell>
@@ -75,13 +51,46 @@ function Row(props) {
           )}
         </TableCell>
       </TableRow>
-      <TableRow style={{ background: "whitesmoke" }}>
+      <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                TEST
-              </Typography>
+              <table style={{ width: "100%" }}>
+                <tr>
+                  <td>Address</td>
+                  <td>
+                    {row.address} {row.city}, {row.province} {row.postalCode}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Budget</td>
+                  <td>${row.budget}</td>
+                </tr>
+                <tr>
+                  <td>Description</td>
+                  <td>{row.description}</td>
+                </tr>
+                <tr>
+                  <td>Last Updated</td>
+                  <td>{row.lastUpdatedDate.split(" ")[0]}</td>
+                </tr>
+              </table>
+              <div className="button-container">
+                <Button
+                  onClick={() => alert("test")}
+                  variant="contained"
+                  color="secondary"
+                >
+                  ACCEPT JOB
+                </Button>
+                <Button
+                  onClick={() => alert("test")}
+                  variant="contained"
+                  color="secondary"
+                >
+                  DECLINE JOB
+                </Button>
+              </div>
             </Box>
           </Collapse>
         </TableCell>
@@ -95,6 +104,7 @@ class JobsPage extends Component {
     jobs: null,
     isLoading: false
   };
+
   componentDidMount() {
     this.setState({ isLoading: true });
     JobService.getJobs()
@@ -106,31 +116,39 @@ class JobsPage extends Component {
         this.setState({ isLoading: false });
       });
   }
+
   render() {
     return (
       <div className="jobs-page-container">
         {this.state.jobs && (
           <Aux>
             <Title>JOBS</Title>
-            <TableContainer style={{ borderRadius: "0" }} component={Paper}>
+            <TableContainer
+              style={{
+                border: "1px solid rgba(224, 224, 224, 1)",
+                borderRadius: "0",
+                boxShadow: "none"
+              }}
+              component={Paper}
+            >
               <Table aria-label="collapsible table">
                 <TableHead>
-                  <TableRow>
+                  <TableRow style={{ backgroundColor: "rgb(243 243 243)" }}>
                     <TableCell />
                     <TableCell>
-                      <b>Service</b>
+                      <b>SERVICE</b>
                     </TableCell>
                     <TableCell>
-                      <b>Address</b>
+                      <b>ADDRESS</b>
                     </TableCell>
                     <TableCell>
-                      <b>City</b>
+                      <b>CITY</b>
                     </TableCell>
                     <TableCell>
-                      <b>Date Created</b>
+                      <b>CREATED</b>
                     </TableCell>
                     <TableCell>
-                      <b>Status</b>
+                      <b>STATUS</b>
                     </TableCell>
                   </TableRow>
                 </TableHead>
@@ -141,40 +159,6 @@ class JobsPage extends Component {
                 </TableBody>
               </Table>
             </TableContainer>
-            {/* <TableContainer component={Paper}>
-              <Table aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Service</TableCell>
-                    <TableCell>Budget</TableCell>
-                    <TableCell>Description</TableCell>
-                    <TableCell>Address</TableCell>
-                    <TableCell>City</TableCell>
-                    <TableCell>Postal Code</TableCell>
-                    <TableCell>Province</TableCell>
-                    <TableCell>Creation Date</TableCell>
-                    <TableCell>Completion Date</TableCell>
-                    <TableCell>Inspection Date</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {this.state.jobs.map(job => (
-                    <TableRow className="table-row" key={job.jobId}>
-                      <TableCell>{job.serviceId}</TableCell>
-                      <TableCell>{job.budget}</TableCell>
-                      <TableCell>{job.description}</TableCell>
-                      <TableCell>{job.address}</TableCell>
-                      <TableCell>{job.city}</TableCell>
-                      <TableCell>{job.postalCode}</TableCell>
-                      <TableCell>{job.province}</TableCell>
-                      <TableCell>{job.creationDate}</TableCell>
-                      <TableCell>{job.completionDate}</TableCell>
-                      <TableCell>{job.inspectionDate}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer> */}
           </Aux>
         )}
         {!this.state.jobs && !this.state.isLoading && (
@@ -188,4 +172,5 @@ class JobsPage extends Component {
     );
   }
 }
+
 export default JobsPage;
